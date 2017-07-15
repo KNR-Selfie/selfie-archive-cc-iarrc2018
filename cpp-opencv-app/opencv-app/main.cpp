289df9c589cbd628248868adda08dbad08c33b02
Unknown <mgmateusz.grudzien@gmail.com>
2017-07-15 23:05:01 +0200

    new

---
+++ cpp-opencv-app/opencv-app/main.cpp
@@ -0,0 +1,19 @@
+#include "camera.cpp"
+#include "GUI.cpp"
+#include "lane_detection.cpp"
+#include "sign_detection.cpp"
+#include "UART.cpp"
+
+int main(int argc, char* argv[])
+{
+    //Setup
+
+
+    //Main loop
+    while(true)
+    {
+
+    }
+
+    return 0;
+}
