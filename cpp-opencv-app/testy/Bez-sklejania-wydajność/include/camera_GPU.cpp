2f9d501519a2079a321ebc68c422a46ae559d81f
mateuszg12 <mgmateusz.grudzien@gmail.com>
2017-07-26 16:41:48 +0200

    n

---
+++ cpp-opencv-app/testy/Bez-sklejania-wydajność/include/camera_GPU.cpp
@@ -0,0 +1,112 @@
+#include "camera_GPU.h"
+
+bool start_camera(int camera_index, unsigned short camera_width, unsigned short camera_height)
+{
+    camera.open(camera_index);
+    
+    if (camera.isOpened())
+    {
+        cout << "Camera opened properly on port 0" << endl;
+	cout << "Resolution: " << camera.get(CV_CAP_PROP_FRAME_WIDTH) << " x " << camera.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
+		
+        return 1;
+    }	
+    else
+    {
+		cout << "ERROR: Could not access camera on port 0" << endl;
+		
+        return 0;
+    }
+}
+
+void destroy_handlers()
+{
+	camera.release();
+    gpu::resetDevice();
+}
+
+bool set_CUDA_device(unsigned short CUDA_device_index)
+{
+    //Check of apropriate GPU module with CUDA enabled
+    if(!gpu::getCudaEnabledDeviceCount())
+    {
+		cout << "No CUDA module detected!" << endl;
+		cout << "Closing app!" << endl;
+		return 0;
+    }
+    else
+    {
+		cout << "Detected " << gpu::getCudaEnabledDeviceCount() << " CUDA device/s" << endl;
+    }
+    
+    //Setting of GPU module on adress 0
+    gpu::setDevice(CUDA_device_index);
+
+    //Check of proper initializing of CUDA device on adress 0
+    gpu::DeviceInfo CUDA_device;
+    if(CUDA_device.deviceID() == 0 && CUDA_device.isCompatible() == true)
+    {
+		cout << "CUDA device initialized properly on adress 0" << endl;
+    }
+    else
+    {
+		cout << "Initializing CUDA device on adress 0 failed!" << endl;
+		cout << "Closing app!" << endl;
+		return 0;
+    }
+
+    //Display info about CUDA device
+	cout << "CUDA device name: " << CUDA_device.name() << endl;
+	cout << "Version: " << CUDA_device.majorVersion() << "." << CUDA_device.minorVersion() << endl;
+	cout << "Number of streaming multiprocessors: " << CUDA_device.multiProcessorCount() << endl;
+	cout << "Total device GDDR5 memory: " << CUDA_device.totalMemory() << " bytes" << endl;
+	cout << "Amount of free GDDR5 memory: " << CUDA_device.freeMemory() << " bytes" << endl;
+	
+	return 1;
+}
+
+Mat get_frame(unsigned short camera_index)
+{
+        Mat frame;	
+
+        camera >> frame;
+	
+	if(!frame.data)
+	{
+		cout << "No image data!" << endl;
+	}
+	
+	return frame;
+}
+
+Mat filter_frame(Mat frame, unsigned short compared_level_main, unsigned short ROI_horizontal_pos, unsigned short ROI_vertical_pos, unsigned short ROI_width, unsigned short ROI_height)
+{
+	Mat frame_ROI, frame_from_gpu;
+	gpu::GpuMat gpu_frame, gpu_hsv, gpu_range_frame, gpu_filtered;
+	vector<gpu::GpuMat> gpu_hsv_split;
+	
+	int erosion_size = 1;  
+	Mat element = getStructuringElement(
+		cv::MORPH_RECT, 
+		cv::Size(2 * erosion_size, 2 * erosion_size), 
+		cv::Point(erosion_size, erosion_size) );
+
+	Rect ROI_rec(
+		ROI_horizontal_pos,
+		ROI_vertical_pos,
+		ROI_width,
+		ROI_height);
+
+	frame(ROI_rec).copyTo(frame_ROI);
+
+	gpu_frame.upload(frame_ROI);
+
+	gpu::cvtColor(gpu_frame, gpu_hsv, CV_BGR2HSV);
+	gpu::split(gpu_hsv, gpu_hsv_split);
+	gpu::threshold(gpu_hsv_split[2], gpu_range_frame, compared_level_main, 255, THRESH_BINARY);
+	dilate(gpu_range_frame, gpu_filtered, element);
+
+	gpu_filtered.download(frame_from_gpu);
+	
+	return frame_from_gpu;
+}
---
+++ cpp-opencv-app/testy/Bez-sklejania-wydajność/main.cpp
@@ -0,0 +1,123 @@
+#include "include/camera_GPU.cpp"
+#include "include/GUI.cpp"
+#include "include/lane_detection.cpp"
+#include "include/sign_detection.cpp"
+#include "include/UART.cpp"
+
+#define CAM_INDEX 0
+#define CUDA_INDEX 0
+#define CAM_WIDTH 640
+#define CAM_HEIGHT 480
+
+Mat real_frame;
+Mat main_ROI_frame;
+
+int compared_level = 115;
+
+int ROI_width = 620, ROI_height = 100;
+int ROI_horizontal_pos = 9, ROI_vertical_pos = 200;
+	
+unsigned short horizontal_start_position_top = ROI_width / 2;
+unsigned short horizontal_start_position_bottom = ROI_width / 2;
+
+unsigned short left_top_pos = 0, right_top_pos = ROI_width - 1;
+unsigned short left_bottom_pos = 0, right_bottom_pos = ROI_width - 1;
+
+bool top_left_line_visible = false;
+bool bottom_left_line_visible = false;
+bool top_right_line_visible = false;
+bool bottom_right_line_visible = false;
+
+unsigned short detected_middle_pos = 320;
+unsigned short detected_middle_top_pos = 0;
+unsigned short detected_middle_bottom_pos = ROI_width;
+
+double bisector_angle_rad = 0.5;
+double bisector_angle_st = 90;
+
+int main(int argc, char** argv)
+{
+    if(!start_camera(CAM_INDEX, CAM_WIDTH, CAM_HEIGHT))
+    {
+        return 0;
+    }
+
+	if(!set_CUDA_device(CUDA_INDEX))
+	{
+		return 0;
+	}
+	
+	if(!UART_set())
+	{
+		return 0;
+	}
+	else
+	{
+		unia_danych.dane.sync_byte = 0xff;
+		unia_danych.dane.flags = 0b00000000;
+		unia_danych.dane.end_byte = 0xff;
+	}
+	
+	wait_for_user();
+	
+	create_windows_trackbars(compared_level, ROI_horizontal_pos, ROI_height);
+	
+    //Main loop
+    while(true)
+    {
+		real_frame = get_frame(CAM_INDEX);
+		main_ROI_frame = filter_frame(real_frame, compared_level, ROI_horizontal_pos, ROI_vertical_pos, ROI_width, ROI_height);
+		
+		search_to_left_top_main_ROI(main_ROI_frame, left_top_pos, top_left_line_visible, ROI_width, ROI_height, horizontal_start_position_top);
+		search_to_right_top_main_ROI(main_ROI_frame, right_top_pos, top_right_line_visible, ROI_width, ROI_height, horizontal_start_position_top);
+		search_to_left_bottom_main_ROI(main_ROI_frame, left_bottom_pos, bottom_left_line_visible, ROI_width, ROI_height, horizontal_start_position_bottom);
+		search_to_right_bottom_main_ROI(main_ROI_frame, right_bottom_pos, bottom_right_line_visible, ROI_width, ROI_height, horizontal_start_position_bottom);
+		
+		horizontal_start_position_top = min(left_top_pos, right_top_pos) + abs(right_top_pos - left_top_pos) / 2;
+		horizontal_start_position_bottom = min(left_bottom_pos, right_bottom_pos) + abs(right_bottom_pos - left_bottom_pos) / 2;;	
+		
+		detected_middle_top_pos = ROI_horizontal_pos + min(left_top_pos, right_top_pos) + abs(left_top_pos - right_top_pos) / 2;
+		detected_middle_bottom_pos = ROI_horizontal_pos + min(left_bottom_pos, right_bottom_pos) + abs(left_bottom_pos - right_bottom_pos) / 2;
+		detected_middle_pos = ROI_horizontal_pos + min(detected_middle_top_pos, detected_middle_bottom_pos) + abs(detected_middle_top_pos - detected_middle_bottom_pos) / 2;
+		
+		calculate_bisector(ROI_height, detected_middle_bottom_pos, detected_middle_top_pos, bisector_angle_rad, bisector_angle_st);
+		
+		//UART part
+		unia_danych.dane.data_0 = 0b11111111111;
+		unia_danych.dane.data_1 = 0b11111111111;
+		unia_danych.dane.data_2 = 0b11111111111;
+		unia_danych.dane.data_3 = 0b11111111111;
+		unia_danych.dane.data_4 = 0b11111111111;
+		unia_danych.dane.data_5 = 0b11111111111;
+		unia_danych.dane.data_6 = 0b11111111111;
+		unia_danych.dane.data_7 = 0b11111111111;
+		unia_danych.dane.flags = 0xff;
+		
+		send_UART_data();
+		//UART part
+		
+		//GUI part
+		cout << "Middle: " << detected_middle_pos << endl;
+		
+		Rect ROI_rec(
+		ROI_horizontal_pos,
+		ROI_vertical_pos,
+		ROI_width,
+		ROI_height);
+		
+		display_main_ROI(real_frame, ROI_rec);
+		display_detected_main_ROI_info(real_frame, ROI_horizontal_pos, ROI_vertical_pos, ROI_width, ROI_height,  left_top_pos,  right_top_pos,  left_bottom_pos,  right_bottom_pos,  detected_middle_top_pos,  detected_middle_bottom_pos,  detected_middle_pos);
+		display_text(real_frame, ROI_horizontal_pos, ROI_vertical_pos, ROI_width, ROI_height, detected_middle_pos, bisector_angle_rad, bisector_angle_st);
+		display_windows(real_frame, main_ROI_frame);
+		//GUI part
+		
+		if (waitKey(50) != -1)
+        {
+            cout << "Key pressed, closing app" << endl;
+            break;
+        }
+    }
+
+	destroy_handlers();
+    return 0;
+}
