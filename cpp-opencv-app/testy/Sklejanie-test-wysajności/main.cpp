41a6f50b424c735e61d09bd5d2bfad1e8e332f51
mateuszg12 <mgmateusz.grudzien@gmail.com>
2017-07-26 14:35:04 +0200

    n

--- cpp-opencv-app/testy/Sklejanie-test-wysajności/main.cpp
+++ cpp-opencv-app/testy/Sklejanie-test-wysajności/main.cpp
@@ -15,7 +15,7 @@ int X_pos = 640;
 
 Mat left_frame(480, 640, CV_8UC3, Scalar(200,255,200));
 Mat right_frame(480, 640, CV_8UC3, Scalar(200,255,200));
-Mat result_frame(480, 1300, CV_8UC3, Scalar(200,255,200));
+Mat result_frame(480, 1280, CV_8UC3, Scalar(200,255,200));
 Mat main_ROI_frame;
 
 int compared_level = 115;
@@ -55,12 +55,16 @@ int main(int argc, char** argv)
     //Main loop
     while(true)
     {
+		cout << "1";
 		camera_left >> left_frame;// = get_frame(camera_left);
+		cout << "1";
 		camera_right >> right_frame;// = get_frame(camera_right);
 		
+		cout << "1";
 		left_frame.copyTo(result_frame(Rect(0, 0, left_frame.cols, left_frame.rows)));
+		cout << "1";
 		right_frame.copyTo(result_frame(Rect(X_pos, 0, left_frame.cols, left_frame.rows)));
-		
+		cout << "1";
 		main_ROI_frame = filter_frame(result_frame, compared_level, ROI_horizontal_pos, ROI_vertical_pos, ROI_width, ROI_height);
 		
 		search_to_left_top_main_ROI(main_ROI_frame, left_top_pos, top_left_line_visible, ROI_width, ROI_height, horizontal_start_position_top);
