e700a38f29266996e2c4ed4e16a3bbaec8eb80b0
Mateusz Percinski <mateusz.percinski@gmail.com>
2017-10-25 18:12:24 +0200

    android app: initial commit

---
+++ android-app/Selfie/app/src/main/java/com/perciax/selfie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.perciax.selfie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
