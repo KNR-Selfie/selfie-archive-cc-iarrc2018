96aa68f5356371de47cec36c553019cfac2c1b1b
Unknown <mice.co@hotmail.com>
2018-02-03 19:14:32 +0100

    Fixy
    Wjazd i wyjazd z parkingu
    Czujniki
    Naprawione cofanie

--- stm32-atollic/SelfieF7/Src/VL53L0X/Czujniki.h
+++ stm32-atollic/SelfieF7/Src/VL53L0X/Czujniki.h
@@ -9,8 +9,10 @@
 #define VL53L0X_CZUJNIKI_H_
 
 #include "stm32f7xx_hal.h"
-
+#include "cmsis_os.h"
 #define VLX_SENSOR_COUNT 8
+
+extern xSemaphoreHandle i2c2_semaphore;
 
 uint16_t range[VLX_SENSOR_COUNT];
 uint16_t lane_change_treshold;
