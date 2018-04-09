78e1c3603ff11853f97f9f82cc7a5bee6bf78104
Unknown <mgmateusz.grudzien@gmail.com>
2018-04-09 16:46:31 +0200

    Adds main loop

--- cpp-opencv-app/opencv-app/opencv-app/main.cpp
+++ cpp-opencv-app/opencv-app/opencv-app/main.cpp
@@ -7,11 +7,17 @@
 
 #include <include/usb.hpp>
 
+#define CAM_RES_X 640
+#define CAM_RES_Y 360
+#define FRAME_TIME 30
+
 USB Usb_STM;
 USB Usb_LIDAR;
 
 int main()
 {
+    cv::Mat frame(CAM_RES_Y, CAM_RES_X, CV_8UC4);
+
     if(Usb_STM.init(B1152000) < 0)
     {
         std::cout << "Closing app!" << std::endl;
@@ -22,6 +28,27 @@ int main()
     {
         std::cout << "Closing app!" << std::endl;
         return -1;
+    }
+
+    while(true)
+    {
+        cv::imshow("Camera", frame);
+
+        keypressed = (char)cv::waitKey(FRAME_TIME);
+
+        if( keypressed == 27 )
+            break;
+
+        switch(keypressed)
+        {
+        case 'q':
+
+            break;
+
+        case 's':
+
+            break;
+        }
     }
 
     return 0;
