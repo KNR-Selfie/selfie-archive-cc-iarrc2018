a5096d2bd66b225d22471ff4346a2589a4aebce2
Unknown <mgmateusz.grudzien@gmail.com>
2018-04-10 17:15:23 +0200

    Adds method initializing new memory block in Unix Shared Memory

--- cpp-opencv-app/opencv-app/opencv-app/include/sharedmemory.cpp
+++ cpp-opencv-app/opencv-app/opencv-app/include/sharedmemory.cpp
@@ -7,7 +7,22 @@ SharedMemory::SharedMemory()
 
 bool SharedMemory::init()
 {
-    return 0;
+    mem_id = shmget(key, MEMSIZE, IPC_CREAT | 0666);
+    if(mem_id < 0)
+    {
+        std::cout << "Error getting memmory access at given key" << std::endl;
+        return 0;
+    }
+
+    shared_variable = (char*) shmat(mem_id, NULL, 0);
+
+    if(shared_variable == (char*) -1)
+    {
+        std::cout << "Error trying to read memory" << std::endl;
+        return 0;
+    }
+
+    return 1;
 }
 
 bool SharedMemory::get_access()
