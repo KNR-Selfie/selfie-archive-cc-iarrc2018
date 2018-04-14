0e9f5539034e90cd6854fa99f88632101667c474
Unknown <mice.co@hotmail.com>
2018-04-14 14:48:45 +0200

    USB transmission with Intel-CPU

---
+++ stm32-atollic/SelfieF7/Src/USB/USBTask.h
@@ -0,0 +1,16 @@
+/*
+ * USBTask.h
+ *
+ *  Created on: 14.04.2018
+ *      Author: mice
+ */
+
+#ifndef USB_USBTASK_H_
+#define USB_USBTASK_H_
+
+extern uint32_t USB_linepos;
+
+
+void StartUSBTask(void const * argument);
+
+#endif /* USB_USBTASK_H_ */
